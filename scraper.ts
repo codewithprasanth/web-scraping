ae2ac814bcf6cdf79834aeb4ce83eafdde3e7b10
Prasanth Ponnusamy <prasanth.ponnusamy@olamnet.com>
2024-02-12 22:17:30 +0530

    with url

--- scraper.ts
+++ scraper.ts
@@ -25,29 +25,38 @@ const scrap = async (whatsappClient: Client) => {
             title: (
               prod.getElementsByClassName("post-title")[0] as HTMLElement
             ).innerText.replace("₹", ""),
-            offerPrice: (
+            currentPrice: (
               prod.getElementsByClassName("off-price")[0] as HTMLElement
             ).innerText,
-            currentPrice: (
+            offerPrice: (
               prod.getElementsByClassName("cur-price")[0] as HTMLElement
             ).innerText,
             discountPercent: Number.parseInt(
               (prod.getElementsByClassName("discount-rb")[0] as HTMLElement)
                 .innerText
             ),
+            url: (
+              (prod.getElementsByClassName("btn-bottom-nav")[0] as HTMLElement)
+                .children[1] as HTMLAnchorElement
+            ).href,
+            imageUrl: (
+              prod.getElementsByClassName(
+                "img-res-thumb"
+              )[0] as HTMLImageElement
+            ).src,
           };
         });
       });
       console.log("evaluation done");
       res
-        .filter((res) => res.discountPercent > 20)
+        .filter((res) => res.discountPercent >= 70)
         .forEach((product) => {
           if (!prodArray.includes(product.title)) {
             prodArray.push(product.title);
             console.log(product);
             whatsappClient.sendMessage(
               "917200632341@c.us",
-              `TITLE: ${product.title}\n OFFER PRICE: ${product.offerPrice}\nMRP PRICE: ${product.currentPrice}\nDISCOUNT: ${product.discountPercent}`
+              `TITLE: ${product.title}\nOFFER PRICE: ${product.offerPrice}\nMRP PRICE: ${product.currentPrice}\nDISCOUNT: ${product.discountPercent}%\nURL:${product.url}\nIMAGE URL:${product.imageUrl}`
             );
           }
         });
